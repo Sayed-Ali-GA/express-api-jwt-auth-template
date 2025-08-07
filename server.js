0a9b17f26ca91990ec3169184d0df51e0a0165b0
sayed-ali-ga <alsayed3li2005@gmail.com>
2025-08-07 10:54:24 +0300

    Add update author

--- server.js
+++ server.js
@@ -62,6 +62,7 @@ const logger = require('morgan');
 const testJwtRouter = require('./controllers/test-jwt');
 const authRouter = require('./controllers/auth');
 const userRouter = require('./controllers/users');
+const hootsRouter = require('./controllers/hoots')
 
 // Middleware
 const verifyToken = require('./middleware/verify-token');
@@ -83,8 +84,9 @@ app.use('/auth', authRouter);
 app.use('/test-jwt', testJwtRouter);
 
 // PROTECTED ROUTES
-app.use(verifyToken);
-app.use('/users', userRouter);
+// app.use(verifyToken);
+app.use('/users', userRouter)
+app.use('/hoots', hootsRouter);
 
 app.listen(3000, () => {
   console.log('The express app is ready!');
